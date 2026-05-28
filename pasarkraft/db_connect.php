15ceeb356145f5635093ae899d9aa1b3c734fe36
feeqree <af03.af30@gmail.com>
2026-05-28 09:53:42 +0800

    Update database connection for remote MySQL hosting

--- pasarkraft/db_connect.php
+++ pasarkraft/db_connect.php
@@ -1,8 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = ""; // Your XAMPP password if any
-$dbname = "pasarkraft_db";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$servername = "sql204.infinityfree.com";
+$username = "if0_42022884";
+$password = "PWp1pzwdti3jW"; // Your XAMPP password if any
+$dbname = "if0_42022884_pasarkraft_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
